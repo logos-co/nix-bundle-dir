697d704ac37b0885107eed3646cce7083353e69b
Iuri Matias <iuri.matias@gmail.com>
2026-03-19 18:15:12 -0400

    follow logos-nix for nix packages

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "Bundle Nix derivations into self-contained directories";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    logos-nix.url = "github:logos-co/logos-nix";
+    nixpkgs.follows = "logos-nix/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, logos-nix }:
     let
       systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
